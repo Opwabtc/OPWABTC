7b4f649dbb6be1a26ba81621c9c381183ec8d8e0
Opwabtc <opwabtc@gmail.com>
2026-03-01 12:42:50 -0300

    fix: s16 TS errors App Layout import + Dashboard React unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { Layout } from './components/Layout'
+import Layout from './components/Layout'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Terms from './pages/Terms'
